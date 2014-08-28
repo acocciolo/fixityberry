398c4a0d6419a7398584c8bb265e5bd0c689f558
Anthony Cocciolo <anthony.cocciolo@gmail.com>
2014-08-28 17:54:13 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,24 +1,22 @@
-sudo apt-get install php5
-sudo apt-get install exfat-fuse
-sudo apt-get install usbmount
-sudo apt-get install fuse ntfs-3g
-sudo apt-get install mysql-server
-sudo apt-get install php5-mysql
-
-sudo apt-get install ssmtp mailutils mpack
+apt-get install php5
+apt-get install exfat-fuse
+apt-get install usbmount
+apt-get install fuse ntfs-3g
+apt-get install mysql-server
+apt-get install php5-mysql
+apt-get install ssmtp mailutils mpack
 
 cd /home/pi
 
 wget https://raw.githubusercontent.com/acocciolo/fixityberry/master/fixity_berry_orig.php
 
-(echo "<?php $email_report_to = "$1"; ?>" ; cat fixity_berry_orig.php) > fixity_berry.php
+(echo "<?php \$email_report_to = \"$1\"; ?>" ; cat fixity_berry_orig.php) > fixity_berry.php
 
 cd /etc/usbmount
-sudo wget https://raw.githubusercontent.com/acocciolo/fixityberry/master/usbmount.conf
-
+wget https://raw.githubusercontent.com/acocciolo/fixityberry/master/usbmount.conf
 
 cd mount.d
-sudo wget https://raw.githubusercontent.com/acocciolo/fixityberry/master/00_create_model_symlink
-
+wget https://raw.githubusercontent.com/acocciolo/fixityberry/master/00_create_model_symlink
 
 cd /usr/share/usbmount
+wget https://raw.githubusercontent.com/acocciolo/fixityberry/master/usbmount
