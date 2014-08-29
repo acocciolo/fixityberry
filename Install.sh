23b7d01f7b20ff323d9f71b8fd94e42d001e4573
Anthony Cocciolo <anthony.cocciolo@gmail.com>
2014-08-29 15:35:20 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 apt-get update
 apt-get -y install php5 exfat-fuse usbmount fuse ntfs-3g ssmtp mailutils mpack
-debconf-set-selections <<< 'mysql-server-5.1 mysql-server/root_password password $2'
-debconf-set-selections <<< 'mysql-server-5.1 mysql-server/root_password_again password $2'
+debconf-set-selections <<< "mysql-server-5.1 mysql-server/root_password password $2"
+debconf-set-selections <<< "mysql-server-5.1 mysql-server/root_password_again password $2"
 apt-get -y install mysql-server php5-mysql
 
 
@@ -10,7 +10,7 @@ cd /home/pi
 
 wget https://raw.githubusercontent.com/acocciolo/fixityberry/master/fixity_berry.sql
 
-mysql --user=root --password="$2" -e "CREATE DATABASE fixity_berry; CREATE USER 'fixity_berry'@'localhost' IDENTIFED BY '$3'; GRANT ALL PRIVILEGES ON *.* TO 'fixity_berry'@'localhost';"
+mysql --user=root --password="$2" -e "CREATE DATABASE fixity_berry; CREATE USER fixity_berry@localhost IDENTIFED BY $3; GRANT ALL PRIVILEGES ON *.* TO fixity_berry@localhost;"
 mysql --user=fixity_berry --password="$3" fixity_berry < fixity_berry.sql
 
 wget https://raw.githubusercontent.com/acocciolo/fixityberry/master/fixity_berry_orig.php
@@ -22,11 +22,14 @@ mv usbmount.conf /etc/usbmount/
 
 wget https://raw.githubusercontent.com/acocciolo/fixityberry/master/00_create_model_symlink
 mv 00_create_model_symlink /etc/usbmount/mount.d/
+chmod ugo+rx /etc/usbmount/mount.d/00_create_model_symlink
 
 wget https://raw.githubusercontent.com/acocciolo/fixityberry/master/usbmount
 mv usbmount /usr/share/usbmount/
+chmod ugo+rx /usr/share/usbmount/usbmount
 
 wget https://raw.githubusercontent.com/acocciolo/fixityberry/master/rc.local
 mv rc.local /etc/
+chmod ugo+x /etc/rc.local
 
 echo -e "mailhub=ssmtp.gmail.com:587\nAuthUser=$4\nAuthPass=$5\nuseSTARTTLS=YES\nFromLineOverride=NO" >> /etc/ssmtp/ssmtp.conf
