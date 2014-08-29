3b68571c091d482d6600f032ba67bc0171a4fb09
Anthony Cocciolo <anthony.cocciolo@gmail.com>
2014-08-29 14:29:18 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -12,19 +12,18 @@ wget https://raw.githubusercontent.com/acocciolo/fixityberry/master/fixity_berry
 mysql --user=root --password="$2" -e "CREATE DATABASE fixity_berry; CREATE USER 'fixity_berry'@'localhost' IDENTIFED BY '$3'; GRANT ALL PRIVILEGES ON *.* TO 'fixity_berry'@'localhost';"
 mysql --user=fixity_berry --password="$3" < fixity_berry.sql
 
-
 wget https://raw.githubusercontent.com/acocciolo/fixityberry/master/fixity_berry_orig.php
 
 (echo "<?php \$email_report_to = \"$1\"; \n mysql_connect(\"localhost\", \"fixity_berry\", \"$3\"); ?>" ; cat fixity_berry_orig.php) > fixity_berry.php
 
-cd /etc/usbmount
 wget https://raw.githubusercontent.com/acocciolo/fixityberry/master/usbmount.conf
+mv usbmount.conf /etc/usbmount/
 
-cd mount.d
 wget https://raw.githubusercontent.com/acocciolo/fixityberry/master/00_create_model_symlink
+mv 00_create_model_symlink /etc/usbmount/mount.d/
 
-cd /usr/share/usbmount
 wget https://raw.githubusercontent.com/acocciolo/fixityberry/master/usbmount
+mv usbmount /usr/share/usbmount/
 
-cd /etc
 wget https://raw.githubusercontent.com/acocciolo/fixityberry/master/rc.local
+mv rc.local /etc/
